6600712382daa8c25ec6a1527f59057c92921300
Muhammad Nassef <muhammadnassef34@gmail.com>
2022-01-14 18:27:47 +0300

    scholarships management app first upload

---
+++ scholarships_management/scholarships_management/doctype/scholarship_request/scholarship_request.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2022, repoteq and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class ScholarshipRequest(Document):
+    def on_submit(self):
+        # sender = 'muhammadnassef34@gmail.com'
+        frappe.sendmail(recipients=[self.email], subject='Security Scanner Forum',
+                        template='Security Scanner Forum', args=dict(full_name_en=self.full_name_en, ))
+
+    def on_trash(self):
+        name = frappe.db.get_value('Security Scanner Forum', {
+                                   'email': f'{self.email}'}, 'name')
+        if not name == None:
+            frappe.db.set_value('Security Scanner Forum', name, 'docstatus', 2)
+            frappe.delete_doc('Security Scanner Forum', name)
