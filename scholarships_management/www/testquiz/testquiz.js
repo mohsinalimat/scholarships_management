8d82794b10cd0d12105bb7bcb2d531ce29eaf36a
Muhammad Nassef <muhammadnassef34@gmail.com>
2022-02-08 13:39:13 +0300

    quiz form update

--- scholarships_management/www/testquiz/testquiz.js
+++ scholarships_management/www/testquiz/testquiz.js
@@ -18,27 +18,43 @@ function get_form_data() {
 }
 
 // Quiz Section..
-
+var quizContent = document.getElementById('quiz-content');
 frappe.call({
     method: "scholarships_management.www.testquiz.testquiz.get_question_option",
     callback: function (r) {
         if (r.message) {
             Object.entries(r.message).forEach(function (value) {
-                let quizContent = document.getElementById('quiz-content');
+
 
                 let questionHeader = document.createElement("h1");
                 questionHeader.innerHTML = value[0];
-                quizContent.append(questionHeader);
-
-                let questionOptions = document.createElement("ul");
+                quizContent.appendChild(questionHeader);
 
                 for (let optn in value[1]) {
-                    let option = document.createElement("li");
-                    option.innerHTML = value[1][optn]
-                    questionOptions.appendChild(option)
+                    // create radio button..
+                    let radioBtn = document.createElement("input");
+                    radioBtn.type = "radio";
+                    radioBtn.name = value[1][0];
+                    radioBtn.id = value[1][optn];
+                    radioBtn.value = value[1][optn];
+                    radioBtn.required = true;
+                    quizContent.appendChild(radioBtn);
+                    // create label for radio button..
+                    let option = document.createElement("label");
+                    let optionTxt = document.createTextNode(value[1][optn]);
+                    option.htmlFor = radioBtn.id;
+                    option.appendChild(radioBtn);
+                    option.appendChild(optionTxt);
+                    quizContent.appendChild(option);
+                    quizContent.appendChild(document.createElement("br"));
                 }
-                quizContent.appendChild(questionOptions)
             });
+            // create submit quiz button
+            let submitBtn = document.createElement("input");
+            submitBtn.type = "submit";
+            submitBtn.value = "Submit Quiz";
+            submitBtn.className = "col-md-3 ml-auto btn btn-block btn-primary text-white py-3 px-5";
+            quizContent.appendChild(submitBtn);
         }
     }
 })
